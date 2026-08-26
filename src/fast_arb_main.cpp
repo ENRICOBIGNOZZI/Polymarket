4e4350a4ce57da602e2dbdca35d42e1d39a6e94a
ENRICOBIGNOZZI <88671442+ENRICOBIGNOZZI@users.noreply.github.com>
2026-08-27 01:20:44 +0200

    Wire V7 Fast Arb prospective PAPER execution model

--- src/fast_arb_main.cpp
+++ src/fast_arb_main.cpp
@@ -1,5 +1,6 @@
 // The implementation is split into ordered include fragments to keep the runtime
 // planes reviewable while compiling as one translation unit.
+#include "pm/fast_paper_execution.hpp"
 #include "fast_runtime/part1.inc"
 #include "fast_runtime/part2.inc"
 #include "fast_runtime/part3.inc"
