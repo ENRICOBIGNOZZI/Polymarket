50d6dbd4d0fd57724998866adc4fc22a597d6b93
ENRICOBIGNOZZI <88671442+ENRICOBIGNOZZI@users.noreply.github.com>
2026-09-22 21:06:01 +0200

    latency: port Stage 1b native trace contract

--- include/pm/v7_market_ws.hpp
+++ include/pm/v7_market_ws.hpp
@@ -39,6 +39,7 @@ struct MarketWsEvent {
     std::int64_t quantity_microunits = 0;
     std::int64_t exchange_event_ns = 0;
     std::int64_t receive_monotonic_ns = 0;
+    std::int64_t decode_complete_monotonic_ns = 0;
     std::int64_t frame_parse_ns = 0;
     std::int64_t book_apply_ns = 0;
     std::int64_t receive_to_book_ns = 0;
