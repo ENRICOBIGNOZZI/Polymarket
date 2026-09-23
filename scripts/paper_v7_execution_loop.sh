cecfe9bb9a46115269261d47762b0d162b5a2d48
ENRICOBIGNOZZI <88671442+ENRICOBIGNOZZI@users.noreply.github.com>
2026-09-23 18:30:44 +0200

    Align launcher child count with exact-arb observers

--- scripts/paper_v7_execution_loop.sh
+++ scripts/paper_v7_execution_loop.sh
@@ -869,7 +869,7 @@ v7_register_child "$!"
   done
 ) & v7_register_child "$!"
 
-v7_assert_registered_child_count 33
+v7_assert_registered_child_count 35
 write_runtime_status running false
 
 while [[ ! -e "$KILL" ]]; do
